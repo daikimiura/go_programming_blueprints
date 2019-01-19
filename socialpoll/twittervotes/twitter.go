9d3d33ee63d45381fe8781deaf8161c879fe21f9
daikimiura <daikiii5555@gmail.com>
2019-01-19 21:45:21 +0900

    5.3.1

---
+++ socialpoll/twittervotes/twitter.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"github.com/garyburd/go-oauth/oauth"
+	"io"
+	"net"
+	"net/http"
+	"net/url"
+	"strconv"
+	"sync"
+	"time"
+)
+
+var conn net.Conn
+
+func dial(netw, addr string) (net.Conn, error) {
+	if conn != nil {
+		conn.Close()
+		conn = nil
+	}
+	netc, err := net.DialTimeout(netw, addr, 5*time.Second)
+	if err != nil {
+		return nil, err
+	}
+	conn = netc
+	return netc, nil
+}
+
+var reader io.ReadCloser
+
+func closeConn() {
+	if conn != nil {
+		conn.Close()
+	}
+	if reader != nil {
+		reader.Close()
+	}
+}
+
+var (
+	authClient *oauth.Client
+	creds      *oauth.Credentials
+)
+
+func setupTwitterAuth() {
+	var ts struct {
+		ConsumerKey    string
+		ConsumerSecret string
+		AccessToken    string
+		AccessSecret   string
+	}
+
+	creds = &oauth.Credentials{
+		Token:  ts.AccessToken,
+		Secret: ts.AccessSecret,
+	}
+
+	authClient = &oauth.Client{
+		Credentials: oauth.Credentials{
+			Token:  ts.ConsumerKey,
+			Secret: ts.ConsumerSecret,
+		},
+	}
+}
+
+var (
+	authSetupOnce sync.Once
+	httpClient    *http.Client
+)
+
+func makeRequest(req *http.Request, params url.Values) (*http.Response, error) {
+	authSetupOnce.Do(func() {
+		setupTwitterAuth()
+		httpClient = &http.Client{
+			Transport: &http.Transport{
+				Dial: dial,
+			},
+		}
+	})
+	formEnc := params.Encode()
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Set("Content-Length", strconv.Itoa(len(formEnc)))
+	req.Header.Set("Auuthoriization", authClient.AuthorizationHeader(creds, "POST", req.URL, params))
+	return httpClient.Do(req)
+}
