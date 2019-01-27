c91fd13bc602a2776b5bc2445ff0ff11b00de53d
daikimiura <daikiii5555@gmail.com>
2019-01-28 00:02:56 +0900

    5.4.2

--- socialpoll/counter/main.go
+++ socialpoll/counter/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"flag"
 	"fmt"
+	"github.com/bitly/go-nsq"
 	"gopkg.in/mgo.v2"
 	"log"
 	"os"
+	"sync"
 )
 
 var fatalErr error
@@ -16,7 +18,7 @@ func fatal(e error) {
 	fatalErr = e
 }
 
-func main(){
+func main() {
 	defer func() {
 		if fatalErr != nil {
 			os.Exit(1)
@@ -30,10 +32,36 @@ func main(){
 		return
 	}
 
-	defer func(){
+	defer func() {
 		log.Println("データベース接続を閉じます...")
 		db.Close()
 	}()
 
 	pollData := db.DB("ballots").C("polls")
+
+	var countsLock sync.Mutex
+	var counts map[string]int
+
+	log.Println("NSQに接続します...")
+	q, err := nsq.NewConsumer("votes", "counter", nsq.NewConfig())
+	if err != nil {
+		fatal(err)
+		return
+	}
+
+	q.AddHandler(nsq.HandlerFunc(func(m *nsq.Message) error {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if counts == nil {
+			counts = make(map[string]int)
+		}
+		vote := string(m.Body)
+		counts[vote]++
+		return nil
+	}))
+
+	if q.ConnectToNSQLookupd("localhost:4161"); err != nil {
+		fatal(err)
+		return
+	}
 }
