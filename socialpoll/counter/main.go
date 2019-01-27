39b3adc91583508d8c932041f7ef32973e873f15
daikimiura <daikiii5555@gmail.com>
2019-01-28 00:18:53 +0900

    5.4.3

--- socialpoll/counter/main.go
+++ socialpoll/counter/main.go
@@ -5,10 +5,14 @@ import (
 	"fmt"
 	"github.com/bitly/go-nsq"
 	"gopkg.in/mgo.v2"
+	"gopkg.in/mgo.v2/bson"
 	"log"
 	"os"
 	"sync"
+	"time"
 )
+
+const updateDuration = 1 * time.Second
 
 var fatalErr error
 
@@ -64,4 +68,35 @@ func main() {
 		fatal(err)
 		return
 	}
+
+	log.Println("NSQ上での投票を待機します...")
+	var updater *time.Timer
+
+	updater = time.AfterFunc(updateDuration, func() {
+		countsLock.Lock()
+		defer countsLock.Unlock()
+		if len(counts) == 0 {
+			log.Println("新しい投票はありません。データベースの更新をスキップします。")
+		} else {
+			log.Println("データベースを更新します。")
+			log.Println(counts)
+			ok := true
+			for option, count := range counts {
+				sel := bson.M{"options": bson.M{"$in": []string{option}}}
+				up := bson.M{"$inc": bson.M{"results." + option: count}}
+				if _, err := pollData.UpdateAll(sel, up); err != nil {
+					log.Println("更新に失敗しました:", err)
+					ok = false
+					continue
+				}
+				counts[option] = 0
+			}
+
+			if ok {
+				log.Println("データベースの更新が完了しました")
+				counts = nil
+			}
+		}
+		updater.Reset(updateDuration)
+	})
 }
