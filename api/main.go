7d7bf2c55492e898c30688f2e76bc4c9f19d1ca8
daikimiura <daikiii5555@gmail.com>
2019-02-02 22:28:21 +0900

    6.6

--- api/main.go
+++ api/main.go
@@ -1,11 +1,33 @@
 package main
 
 import (
+	"flag"
+	"github.com/stretchr/graceful"
 	"gopkg.in/mgo.v2"
+	"log"
 	"net/http"
+	"time"
 )
 
 func main() {
+	var (
+		addr  = flag.String("addr", ":8080", "エンドポイントのアドレス")
+		mongo = flag.String("mongo", "localhost", "MongoDBのアドレス")
+	)
+	flag.Parse()
+	log.Println("MongoDBに接続します")
+
+	db, err := mgo.Dial(*mongo)
+	if err != nil {
+		log.Fatalln("MongoDBへの接続に失敗しました", err)
+	}
+
+	defer db.Close()
+	mux := http.NewServeMux()
+	mux.HandleFunc("/polls/", withCORS(withVars(withData(db, withAPIKey(handlePolls)))))
+	log.Println("Webサーバーを開始します:", *addr)
+	graceful.Run(*addr, 1*time.Second, mux)
+	log.Println("停止します...")
 }
 
 func withAPIKey(fn http.HandlerFunc) http.HandlerFunc {
@@ -23,7 +45,7 @@ func isValidAPIKey(key string) bool {
 }
 
 func withData(d mgo.Session, fn http.HandlerFunc) http.HandlerFunc {
-	return func (w http.ResponseWriter, r *http.Request) {
+	return func(w http.ResponseWriter, r *http.Request) {
 		thisDb := d.Copy()
 		defer thisDb.Close()
 		SetVar(r, "db", thisDb.DB("ballots"))
