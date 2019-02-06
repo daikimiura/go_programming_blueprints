4d78d78f62691f7acf134e9eb2aa7d0c280b0d4c
daikimiura <daikiii5555@gmail.com>
2019-02-07 02:18:16 +0900

    6.7.2.2

--- api/polls.go
+++ api/polls.go
@@ -19,25 +19,25 @@ func handlePolls(w http.ResponseWriter, r *http.Request) {
 		handlePollsGet(w, r)
 		return
 	case "POST":
-		handlerPollsPost(w, r)
+		handlePollsPost(w, r)
 		return
 	case "DELETE":
 		handlePollsDelete(w, r)
 		return
 	}
 
-	respondHTTPErr(w,r ,http.StatusNotFound)
+	respondHTTPErr(w, r, http.StatusNotFound)
 }
 
 func handlePollsGet(w http.ResponseWriter, r *http.Request) {
-	db := GetVar(r, "db").(*mgo.Detabase)
+	db := GetVar(r, "db").(*mgo.Database)
 	c := db.C("polls")
 	var q *mgo.Query
 	p := NewPath(r.URL.Path)
 	if p.HasID() {
 		q = c.FindId(bson.ObjectIdHex(p.ID))
 	} else {
-		q =  c.Find(nil)
+		q = c.Find(nil)
 	}
 
 	var result []*poll
@@ -48,3 +48,21 @@ func handlePollsGet(w http.ResponseWriter, r *http.Request) {
 	respond(w, r, http.StatusOK, &result)
 }
 
+func handlePollsPost(w http.ResponseWriter, r *http.Request) {
+	db := GetVar(r, "db").(*mgo.Database)
+	c := db.C("polls")
+	var p poll
+	if err := decodeBody(r, &p); err != nil {
+		respondErr(w, r, http.StatusBadRequest, "リクエストから調査項目を読み込めません", err)
+		return
+	}
+
+	p.ID = bson.NewObjectId()
+	if err := c.Insert(p); err != nil {
+		respondErr(w, r, http.StatusInternalServerError, "調査項目の格納に失敗しました", err)
+		return
+	}
+
+	w.Header().Set("Location", "polls/"+p.ID.Hex())
+	respond(w, r, http.StatusCreated, nil)
+}
