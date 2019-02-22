833ec8b97ff09422be505a34f13c4981145b5d6e
daikimiura <daikiii5555@gmail.com>
2019-02-23 00:41:32 +0900

    8.3.2.1

---
+++ backup/cmds/backup/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"encoding/json"
+	"errors"
+	"flag"
+	"fmt"
+	"github.com/matryer/filedb"
+	"log"
+	"strings"
+)
+
+type path struct {
+	Path string
+	Hash string
+}
+
+func (p path) String() string {
+	return fmt.Sprintf("%s [%s]", p.Path, p.Hash)
+}
+
+func main() {
+	var fatalErr error
+	defer func() {
+		if fatalErr != nil {
+			flag.PrintDefaults()
+			log.Fatalln()
+		}
+	}()
+
+	var(
+		dbpath = flag.String("db", "./backupdata",  "データベースのディレクトリへのパス")
+	)
+
+	flag.Parse()
+	args := flag.Args()
+
+	if len(args) < 1{
+		fatalErr  = errors.New("エラー; コマンドを指定してください")
+		return
+	}
+
+	db, err := filedb.Dial(*dbpath)
+	if err != nil {
+		fatalErr = err
+		return
+	}
+
+	defer db.Close()
+	col, err := db.C("paths")
+	if err != nil {
+		fatalErr = err
+		return
+	}
+
+	switch strings.ToLower(args[1]) {
+	case "list":
+		var path path
+	col.ForEach(func(i int, data []byte) bool{
+		err := json.Unmarshal(data, &path)
+		if err != nil {
+			fatalErr = err
+			return true
+		}
+		fmt.Printf("= %s\n", path)
+		return false
+	})
+	case "add":
+	case "remove":
+	}
+}
